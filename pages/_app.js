c61b5708c21a7e59f1c0dd54b6aaf174eadac4ff
Simplecub <exdimail@gmail.com>
2022-10-04 16:02:23 +0400

    add google font

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,14 @@ import '../styles/globals.scss'
 import Layout from "../components/Layout";
 import Image from "next/image";
 import logoA from "../public/image.png"
+import Head from "next/head";
+
 
 const MyApp = ({Component, pageProps}) => (
     <Layout>
+        <Head>
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/>
+        </Head>
         <main>
             <Component {...pageProps} />
         </main>
