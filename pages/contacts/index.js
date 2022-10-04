dd105d567d9f32029ca2cc0e188e14d699b7b86d
Simplecub <exdimail@gmail.com>
2022-10-04 16:16:03 +0400

    add fetch on client

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,13 +1,31 @@
-import Heading from "../../components/Heading";
 import Head from "next/head";
+import {useState, useEffect} from "react";
+import Heading from "../../components/Heading";
 
-const Contacts = () => (
+const Contacts = () => {
+    const [contacts, setContacts] = useState(null);
+    useEffect(()=> {
+        const fetchData = async () => {
+const  response = await fetch ('https://jsonplaceholder.typicode.com/users')
+            const data = await response.json()
+            setContacts(data)
+        }
+        fetchData()
+
+    }, [])//ПУСТОЙ МАССИВ означает что эффект сработает один раз при загрузке компонента
+    return (
     <>
         <Head>
             <title>Contacts</title>
         </Head>
     <Heading text="Contacts list:!"/>
+        <ul>
+            {contacts && contacts.map(({id, name, email}) => (
+                <li key={id}><strong>{name}</strong> ({email})</li>
+            ))}
+        </ul>
     </>
-)
+    )
+}
 
 export default Contacts
