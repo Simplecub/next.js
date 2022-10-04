d529392764823264ec0666935a6912c7c260c83a
Simplecub <exdimail@gmail.com>
2022-10-04 16:21:46 +0400

    add getStaticProps geting data on page in server

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -2,29 +2,28 @@ import Head from "next/head";
 import {useState, useEffect} from "react";
 import Heading from "../../components/Heading";
 
-const Contacts = () => {
-    const [contacts, setContacts] = useState(null);
-    useEffect(()=> {
-        const fetchData = async () => {
-const  response = await fetch ('https://jsonplaceholder.typicode.com/users')
-            const data = await response.json()
-            setContacts(data)
-        }
-        fetchData()
+export const getStaticProps = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    return {
+        props: {contacts: data}
+    }
+}
 
-    }, [])//ПУСТОЙ МАССИВ означает что эффект сработает один раз при загрузке компонента
+const Contacts = ({contacts}) => {
+
     return (
-    <>
-        <Head>
-            <title>Contacts</title>
-        </Head>
-    <Heading text="Contacts list:!"/>
-        <ul>
-            {contacts && contacts.map(({id, name, email}) => (
-                <li key={id}><strong>{name}</strong> ({email})</li>
-            ))}
-        </ul>
-    </>
+        <>
+            <Head>
+                <title>Contacts</title>
+            </Head>
+            <Heading text="Contacts list:!"/>
+            <ul>
+                {contacts && contacts.map(({id, name, email}) => (
+                    <li key={id}><strong>{name}</strong> ({email})</li>
+                ))}
+            </ul>
+        </>
     )
 }
 
